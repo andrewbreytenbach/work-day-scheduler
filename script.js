dce77c4c4dab68f750cbd1faa0572756b7022b10
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-02-22 13:57:28 -0500

    began to add some comments to the html file

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
 
 // Get all the save buttons
