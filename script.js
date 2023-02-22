b41d39e1718dcb08f98413f56c1fb49e6af98cb8
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-02-22 13:43:58 -0500

    This gets all the time-blocks on the page and assigns them to a new variable.

--- script.js
+++ script.js
@@ -2,12 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
 // Get all the save buttons
 const saveButtons = document.querySelectorAll(".saveBtn");
@@ -52,18 +46,17 @@ saveButtons.forEach(function(saveButton) {
       }
       });
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+
+  // This gets all the time-blocks on the page and assigns them to a new variable.
+  const timeBlocks = document.querySelectorAll(".time-block");
 
 
-  // Get the current time and date using Day.js
+  // This gets the current time and date using Day.js
     const currentTime = () => {
       return dayjs().format("dddd MMMM DD, YYYY");
     };
 
-    // Update the current time every second
+    // This will update the current time every second
     setInterval(() => {
       document.getElementById("currentDay").textContent = currentTime();
     }, 1000);
